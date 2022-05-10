af40e4c40845b8f467523e1790fde55509907256
Joao Paulo Dias <jpdias1997@hotmail.com>
2022-05-10 16:34:23 -0300

    chore: update playwright configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,11 +26,15 @@ const config: PlaywrightTestConfig = {
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 5,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "github" : "html",
+  reporter: [
+    ["html", { open: "never", outputFolder: "./results/reports" }],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    video: "on",
+    screenshot: "only-on-failure",
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -65,7 +69,7 @@ const config: PlaywrightTestConfig = {
   ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  outputDir: "test-results/",
+  outputDir: "./results/",
 };
 
 export default config;
